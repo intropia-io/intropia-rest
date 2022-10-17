3a30059b03b546cb1ba7a1400227fff52838a628
Oleksii Sysoiev <bostonleek@gmail.com>
2022-10-17 16:26:11 +0300

    update styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import dynamic from 'next/dynamic';
 import Head from 'next/head';
-import 'swagger-ui-react/swagger-ui.css';
 
 const SwaggerUI = dynamic<{ url: string }>(import((`swagger-ui-react`)), {
     ssr: false,
@@ -13,6 +12,7 @@ export default function Index() {
                 <title>tr3butor REST api</title>
                 <meta name="description" content="tr3butor REST api service"/>
                 <link rel="icon" href="/favicon.ico"/>
+                <link href="main.css" rel="stylesheet" />
             </Head>
             <SwaggerUI url="/swagger.json"/>
         </div>

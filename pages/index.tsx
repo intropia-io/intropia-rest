38b8f538b13a1069131e8f2ddac4f9c5f5a5138b
Oleksii Sysoiev <bostonleek@gmail.com>
2022-11-28 21:00:44 +0200

    update title

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ export default function Index() {
         <div>
             <Head>
                 <title>tr3butor REST api</title>
-                <meta name="description" content="tr3butor REST api service"/>
+                <meta name="description" content="rest API service for tr3butor"/>
                 <link rel="icon" href="/favicon.ico"/>
                 <link href="main.css" rel="stylesheet" />
             </Head>

bfd5fafb43388768bd1aa7c19c75d76cd9f26fd0
Oleksii Sysoiev <bostonleek@gmail.com>
2022-12-02 19:47:33 +0200

    update cors

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
           { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, X-UserSession" },
+          { key: "Access-Control-Allow-Headers", value: "Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, X-UserSession, Authorization" },
         ]
       }
     ]

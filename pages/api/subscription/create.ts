7bc14b3045b23fd2e9647b3410e5f30e5692aa09
Oleksii Sysoiev <bostonleek@gmail.com>
2023-01-05 20:45:50 +0200

    fix error

--- pages/api/subscription/create.ts
+++ pages/api/subscription/create.ts
@@ -43,7 +43,7 @@ export default async function handler(
 
     const { userId, firstName, lastName, username, dynasty, questTypes, eventTypes, reffProgram, updateFrequency } = req.body;
 
-    const user = await prisma.botSubscription.findUnique({ where: { id: userId.toString() } });
+    const user = await prisma.botSubscription.findUnique({ where: { userId: userId.toString() } });
 
     const types = await prisma.botSubscription.upsert({
         where: {

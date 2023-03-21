79be833434f32d56c282963b6801ebf36ec3c492
Oleksii Sysoiev <bostonleek@gmail.com>
2023-03-21 22:22:55 +0200

    add apply select

---
+++ pages/api/apply/[id].ts
@@ -0,0 +1,80 @@
+/**
+ * @swagger
+ * /api/apply/{id}:
+ *   post:
+ *     tags: [Apply]
+ *     description: Get apply by id
+ *     parameters:
+ *       - name: id
+ *         description: id of apply
+ *         in: path
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: success result
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 $ref: '#/components/schemas/Apply'
+ */
+
+import { prisma } from "@intropia-io/prisma-schema";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { hasRights } from "../../../prisma/hasRights";
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<any>
+) {
+    if (req.method === 'OPTIONS') {
+        res.status(200).end()
+        return
+    }
+    const token = req.headers.authorization?.split(" ")[1];
+    const _hasRights = await hasRights({ token: token!, type: "INHOUSE" });
+    if (!_hasRights)
+        return res.status(400).json({ error: "auth not correct" });
+
+    const {
+        query: { id }
+    } = req;
+
+
+    const apply = await prisma.apply.findFirst({
+        select: {
+            id: true,
+            createdAt: true,
+            link: true,
+            historyStatus: {
+                select: {
+                    status: true,
+                    createdAt: true,
+                },
+                orderBy: {
+                    createdAt: 'desc',
+                },
+                take: 1,
+            },
+            contactEmail: true,
+            user: {
+                select: {
+                    id: true,
+                    name: true,
+                    email: true,
+                    image: true,
+                    resumeLink: true,
+                },
+            },
+            cv: true,
+        },
+        where: {
+            id: id?.toString(),
+
+        },
+    });
+    return res.status(200).json(apply);
+}

12b28a69e07b9ba17efa3d98ddf312da200da4d0
Oleksii Sysoiev <bostonleek@gmail.com>
2023-03-21 22:00:18 +0200

    add status update

---
+++ pages/api/apply/status/[id].ts
@@ -0,0 +1,63 @@
+/**
+ * @swagger
+ * /api/apply/status/{id}:
+ *   post:
+ *     tags: [Apply]
+ *     description: Change status of apply
+ *     parameters:
+ *       - name: id
+ *         description: id of apply
+ *         in: query
+ *         required: true
+ *         type: string
+ *       - name: status
+ *         description: status
+ *         in: query
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: success result
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 $ref: '#/components/schemas/Apply'
+ */
+
+import type { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "@intropia-io/prisma-schema";
+import { hasRights } from "../../../../prisma/hasRights";
+import { ApplyHistoryStatus } from "@prisma/client";
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<any>
+) {
+    if (req.method === 'OPTIONS') {
+        res.status(200).end()
+        return
+    }
+    const token = req.headers.authorization?.split(" ")[1];
+    const _hasRights = await hasRights({ token: token!, type: "INHOUSE" });
+    if (!_hasRights)
+        return res.status(400).json({ error: "auth not correct" });
+
+    const {
+        query: { id, status }
+    } = req;
+
+    const apply = await prisma.applyStatusHistory.create({
+        data: {
+            status: status?.toString() as ApplyHistoryStatus || undefined,
+            apply: {
+                connect: {
+                    id: id?.toString() || undefined
+                }
+            }
+        }
+    });
+    return res.status(200).json(apply);
+}

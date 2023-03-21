329ffa068f56cd2f6830bead92de81762e513029
Oleksii Sysoiev <bostonleek@gmail.com>
2023-03-22 00:54:12 +0200

    update

--- pages/api/quest.ts
+++ pages/api/quest.ts
@@ -159,140 +159,137 @@ export default async function handler(
             if (withApply)
                 res.status(200).json(quests.filter((quest: any) => quest.apply.filter((a: any) => applyStatuses?.find(status => status === a.historyStatus[0].status)).length > 0).length)
             else
-                return res.status(200).json(quests.filter((quest: any) => quest.apply.length > 0).length);
+                res.status(200).json(quests.filter((quest: any) => quest.apply.length > 0).length);
         }
 
-
-
-
-
-
-        const quests = await prisma.quests.findMany({
-            take: take ? parseInt(take.toString()) : 100,
-            skip: skip ? parseInt(skip.toString()) : undefined,
-            select: {
-                id: true,
-                title: true,
-                description: true,
-                dynasty: true,
-                token: true,
-                type: true,
-                linkApply: true,
-                rewardFrom: true,
-                rewardTo: true,
-                textBlocks: true,
-                reffReward: true,
-                createdAt: true,
-                updatedAt: true,
-                organization: true,
-                state: true,
-                tokenReward: true,
-                tags: true,
-                apply: withApply && applyStatuses ? {
-                    select: {
-                        historyStatus: {
-                            select: {
-                                status: true,
-                                createdAt: true,
+        else {
+            const quests = await prisma.quests.findMany({
+                take: take ? parseInt(take.toString()) : 100,
+                skip: skip ? parseInt(skip.toString()) : undefined,
+                select: {
+                    id: true,
+                    title: true,
+                    description: true,
+                    dynasty: true,
+                    token: true,
+                    type: true,
+                    linkApply: true,
+                    rewardFrom: true,
+                    rewardTo: true,
+                    textBlocks: true,
+                    reffReward: true,
+                    createdAt: true,
+                    updatedAt: true,
+                    organization: true,
+                    state: true,
+                    tokenReward: true,
+                    tags: true,
+                    apply: withApply && applyStatuses ? {
+                        select: {
+                            historyStatus: {
+                                select: {
+                                    status: true,
+                                    createdAt: true,
+                                },
+                                orderBy: {
+                                    createdAt: "desc",
+                                },
+                                take: 1,
                             },
-                            orderBy: {
-                                createdAt: "desc",
-                            },
-                            take: 1,
                         },
-                    },
-                    where: {
-                        historyStatus: {
-                            some: {
-                                status: {
-                                    in: applyStatuses
+                        where: {
+                            historyStatus: {
+                                some: {
+                                    status: {
+                                        in: applyStatuses
+                                    }
                                 }
                             }
                         }
-                    }
-                } : undefined,
-            },
-            where: {
-                organization: { contractAddress: { contains: contractAddress ? contractAddress.toString() : undefined }, id: { equals: instituteId ? instituteId.toString() : undefined } },
-                type: { id: type ? type.toString() : undefined },
-                title: { contains: search ? search.toString() : undefined },
-                state: state ? state.toString() as EntityStates : undefined,
-                reffReward: rewards === "true" ? { gt: 0 } : rewards === "false" ? null : undefined,
-                apply: withApply ? {
-                    some: {
-                        historyStatus: {
-                            some: {
-                                status: {
-                                    in: applyStatuses
-                                }
-                            }
-                        }
-                    }
-                } : undefined,
-
-            },
-            orderBy: [
-                {
-                    createdAt: sort === "asc" ? sort : "desc",
+                    } : undefined,
                 },
-            ]
-        })
-        return res.status(200).json(quests)
+                where: {
+                    organization: { contractAddress: { contains: contractAddress ? contractAddress.toString() : undefined }, id: { equals: instituteId ? instituteId.toString() : undefined } },
+                    type: { id: type ? type.toString() : undefined },
+                    title: { contains: search ? search.toString() : undefined },
+                    state: state ? state.toString() as EntityStates : undefined,
+                    reffReward: rewards === "true" ? { gt: 0 } : rewards === "false" ? null : undefined,
+                    apply: withApply ? {
+                        some: {
+                            historyStatus: {
+                                some: {
+                                    status: {
+                                        in: applyStatuses
+                                    }
+                                }
+                            }
+                        }
+                    } : undefined,
+
+                },
+                orderBy: [
+                    {
+                        createdAt: sort === "asc" ? sort : "desc",
+                    },
+                ]
+            })
+            res.status(200).json(quests)
+        }
     }
     else if (req.method === 'POST') {
         const _hasRights = await hasRights({ token: token!, type: "INHOUSE" });
         if (!_hasRights)
-            return res.status(400).json({ error: "auth not correct" });
+            res.status(400).json({ error: "auth not correct" });
 
+        else {
+            const createdItem = await prisma.quests.create({
+                data: {
+                    title: req.body.title,
+                    description: req.body.description,
+                    tags: {
+                        connect:
+                            req.body.tags?.split(",").map((tag: string) => ({
+                                id: tag,
+                            })),
+                    },
 
-        const createdItem = await prisma.quests.create({
-            data: {
-                title: req.body.title,
-                description: req.body.description,
-                tags: {
-                    connect:
-                        req.body.tags?.split(",").map((tag: string) => ({
-                            id: tag,
-                        })),
-                },
+                    reffLink: req.body.reffLink,
+                    tokenReward: {
+                        connect: {
+                            id: req.body.tokenReward,
+                        },
+                    },
+                    reffReward: req.body.reffReward ? Number(req.body.reffReward) : null,
+                    state: req.body.state,
+                    organization: {
+                        connect: {
+                            id: req.body.organization,
+                        },
+                    },
+                    dynasty: {
+                        connect: {
+                            id: req.body.dynasty,
+                        },
+                    },
 
-                reffLink: req.body.reffLink,
-                tokenReward: {
-                    connect: {
-                        id: req.body.tokenReward,
+                    type: {
+                        connect: {
+                            id: req.body.type,
+                        },
                     },
-                },
-                reffReward: req.body.reffReward ? Number(req.body.reffReward) : null,
-                state: req.body.state,
-                organization: {
-                    connect: {
-                        id: req.body.organization,
+                    linkApply: req.body.linkApply,
+                    token: {
+                        connect: {
+                            id: req.body.token,
+                        },
                     },
-                },
-                dynasty: {
-                    connect: {
-                        id: req.body.dynasty,
-                    },
-                },
-
-                type: {
-                    connect: {
-                        id: req.body.type,
-                    },
-                },
-                linkApply: req.body.linkApply,
-                token: {
-                    connect: {
-                        id: req.body.token,
-                    },
-                },
-                rewardFrom: req.body.rewardFrom ? parseInt(req.body.rewardFrom) : null,
-                rewardTo: req.body.rewardTo ? parseInt(req.body.rewardTo) : null,
-                textBlocks: req.body.textBlocks,
-            }
-        });
-
-        return res.status(200).json(createdItem)
+                    rewardFrom: req.body.rewardFrom ? parseInt(req.body.rewardFrom) : null,
+                    rewardTo: req.body.rewardTo ? parseInt(req.body.rewardTo) : null,
+                    textBlocks: req.body.textBlocks,
+                }
+            });
+            res.status(200).json(createdItem)
+        }
 
     }
 }
